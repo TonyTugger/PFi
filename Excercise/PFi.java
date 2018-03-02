b004d42174eb4695a0b1b5b2966dad3939ad77c2
misterflex3 <33148464+misterflex3@users.noreply.github.com>
2018-03-02 14:04:19 +0100

    Update PFi.java

--- Excercise/PFi.java
+++ Excercise/PFi.java
@@ -17,13 +17,14 @@ public class Library {
 		Name[0] = "Daniel O' Connor Rented:";
 		Name[1] = "Sebastian Martin Young Rented:";
 		
-		String[] LibraryInv = new String[6];
+		String[] LibraryInv = new String[7];
 		LibraryInv[0] = "Moby Dick";
 		LibraryInv[1] = "BraveHeart" 		;
 		LibraryInv[2] = "Book of the Dead";
 		LibraryInv[3] = "How to Program (cheat edition)";
 		LibraryInv[4] = "The Book of Mormon";
 		LibraryInv[5] = "Bukake and Buggerism";
+		LibraryInv[6] = "Dragon Ball Volume 1";
 		System.out.println(Name[0] + LibraryInv[3]);
 	
 	}
